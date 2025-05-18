f18f3e7c33b53bf3561362bd3623da9ab3a8b9d5
Umuthan Özel <ozelumuthan@gmail.com>
2025-05-18 17:48:45 +0300

    Add item equality operators

--- Elemental Realms/Assets/Scripts/Game/Data/ItemInstance.cs
+++ Elemental Realms/Assets/Scripts/Game/Data/ItemInstance.cs
@@ -7,5 +7,36 @@ namespace Game.Data
     {
         public Item Item;
         public int Durability;
+
+        public static bool operator ==(ItemInstance a, ItemInstance b)
+        {
+            if (ReferenceEquals(a, b))
+                return true;
+
+            if (a is null || b is null)
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(ItemInstance a, ItemInstance b)
+        {
+            return !(a == b);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is ItemInstance other)
+            {
+                return !(Item is ToolItem) && Item.Id == other.Item.Id;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return GetHashCode();
+        }
     }
 }
